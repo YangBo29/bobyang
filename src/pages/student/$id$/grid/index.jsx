05fa91b557aabd47b2b3bdc58a56fb5b029b5bc7
bo.yang@raykite.com <bo.yang@raykite.com>
2020-09-18 19:21:35 +0800

    20200918

--- src/pages/student/$id$/grid/index.jsx
+++ src/pages/student/$id$/grid/index.jsx
@@ -3,6 +3,7 @@ import styles from './index.less';
 
 export default function Grid(props) {
     const list = useRef([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
     return (
         <>
             <div className={styles.gird_content}>
